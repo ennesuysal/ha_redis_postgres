aca504c141b11f05cf38ca2893b9bdee3e301514
Enes Uysal <info.enesuysal@gmail.com>
2021-03-02 22:59:44 +0300

    patch

--- init.sh
+++ init.sh
@@ -1,8 +1,9 @@
 mkdir -pv /mnt/pv-data/redis-sentinel
 cp -v configs/sentinel.conf /mnt/pv-data/redis-sentinel
-create -f configs/configMap.yaml
-create -f volumes/pv_db.yaml
-create -f volumes/pv_redis_sentinel.yaml
-create -f services/postgres_primary.yaml
-create -f services/postgres_replica.yaml
-create -f services/redis-master.yaml
+kubectl create -f configs/configMap.yaml
+kubectl create -f volumes/pv_db.yaml
+kubectl create -f volumes/pv_redis_sentinel.yaml
+kubectl create -f services/postgres_primary.yaml
+kubectl create -f services/postgres_replica.yaml
+kubectl create -f services/redis-master.yaml
+kubectl create -f services/redis-slave.yaml
